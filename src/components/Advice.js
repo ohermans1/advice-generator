67cc14bc3f052ec885c2f3ae80a7d715dedffcf4
Ollie <ohermans1@gmail.com>
2022-03-15 21:29:02 +1100

    Build deployed

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -1,10 +1,19 @@
 import "./Advice.scss";
 
 const Advice = props => {
+  let advice = "A wise person once said, you must first click a button before you receive advice.";
+  if (props.sendAdvice.advice !== undefined) {
+    advice = props.sendAdvice.advice;
+  }
+  let id = "000";
+  if (props.sendAdvice.id !== undefined) {
+    id = props.sendAdvice.id;
+  }
+
   return (
     <div className="advice">
-      <h2 className="advice__header">Advice #{props.sendAdvice.id}</h2>
-      <div className="advice__text">"{props.sendAdvice.advice}"</div>
+      <h2 className="advice__header">Advice #{id}</h2>
+      <div className="advice__text">"{advice}"</div>
     </div>
   );
 };
